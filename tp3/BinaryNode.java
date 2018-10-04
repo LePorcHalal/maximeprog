cbc7876a9660be5f5320dafb6437c55534bb3a80
Vincent Angell <vianga@l3818-03.info.polymtl.ca>
2018-10-04 16:38:46 -0400

    Exercice 1 commence

--- tp3/BinaryNode.java
+++ tp3/BinaryNode.java
@@ -8,7 +8,7 @@ public class BinaryNode<T extends Comparable<? super T> > {
     // TODO: initialisation
     // O(1)
     public BinaryNode(T data) {
-    	data = null;
+    	this.data = data;
     	right = null;
     	left = null;
     }
@@ -22,14 +22,38 @@ public class BinaryNode<T extends Comparable<? super T> > {
     // TODO: on ajoute une nouvelle donnee au bon endroit
     // O(log(n))
     public void insert(T item) {
-    	if(data.compareTo(item) <= 0) {
-    		
-    	}
+    		if(getData()==null)
+    			data = item;
+    		else {
+    		int compareResult = item.compareTo( data );
+    		if(compareResult <= 0 )
+    		left.insert(item);
+    		else if( compareResult > 0 )
+    		right.insert(item);
+    		}
     }
 
     // TODO: est-ce que l'item fais partie du noeuds courant
     // O(log(n))
     public boolean contains(T item) {
+    	if(data.compareTo(item)== 0) {
+    		return true;
+    	}
+    	
+    	if(data.compareTo(item) < 0) {
+    		if(left.getData()==null) {
+    			return false;
+    			
+    		}
+    		data = left.getData(); 
+    		contains(item);
+    	}else {
+    		if(right.getData()==null) {
+    			return false;
+    		}
+    		data = right.getData(); 
+    		contains(item);
+    	}
         return false;
     }
 
@@ -37,7 +61,7 @@ public class BinaryNode<T extends Comparable<? super T> > {
     // O(n)
     public int getHeight() {
     	if( data == null )
-    		return -1;
+    		return 0;
     	else
     		return 1 + Math.max(left.getHeight(),right.getHeight());
     }
@@ -46,6 +70,13 @@ public class BinaryNode<T extends Comparable<? super T> > {
     // de manière que le plus petit item sera le premier inseré
     // O(n)
     public void fillListInOrder(List<BinaryNode<T>> result) {
-
+    	if(left.getData()!=null) {
+    		left.fillListInOrder(result);
+    	}
+    		result.add(this);
+    	if(right.getData()!=null) {
+    		right.fillListInOrder(result);
+    	}
+    		
     }
 }
