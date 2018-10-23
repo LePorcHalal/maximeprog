36aa090f6aa89454b502c776b6fe3b738b9e78eb
Vincent Angell <vangell@videotron.ca>
2018-10-22 20:09:34 -0400

    Exo 1 marche sauf les complexités

--- tp3/BinaryNode.java
+++ tp3/BinaryNode.java
@@ -1,74 +1,90 @@
 import java.util.List;
 
-public class BinaryNode<T extends Comparable<? super T> > {
-    private T data;
-    private BinaryNode<T> right;
-    private BinaryNode<T> left;
+public class BinaryNode<T extends Comparable<? super T>> {
+	private T data;
+	private BinaryNode<T> right;
+	private BinaryNode<T> left;
 
-    // TODO: initialisation
-    // O(1)
-    public BinaryNode(T data) {
-    	data = null;
-    	right = null;
-    	left = null;
-    }
+	// TODO: initialisation
+	// O(1)
+	public BinaryNode(T data) {
+		this.data = data;
+		right = null;
+		left = null;
+	}
 
-    // TODO: on retourne la donnee voulue
-    // O(1)
-    public T getData() {
-        return data;
-    }
+	// TODO: on retourne la donnee voulue
+	// O(1)
+	public T getData() {
+		return data;
+	}
 
-    // TODO: on ajoute une nouvelle donnee au bon endroit
-    // O(log(n))
-    public void insert(T item) {
-    	if(data.compareTo(item) <= 0) {
-    		
-    	}
-    }
+	// TODO: on ajoute une nouvelle donnee au bon endroit
+	// O(log(n))
+	public void insert(T item) {
+		if (data != null) {
+			if (item.compareTo(data) > 0) {
+				if (right != null)
+					right.insert(item);
+				else
+					right = new BinaryNode<T>(item);
+			} else {
+				if (left != null)
+					left.insert(item);
+				else
+					left = new BinaryNode<T>(item);
+			}
+		} else
+			this.data = item;
+	}
 
-    // TODO: est-ce que l'item fais partie du noeuds courant
-    // O(log(n))
-    public boolean contains(T item) {
-    	
-    	if(data.compareTo(item)== 0) {
-    		return true;
-    	}
-    	
-    	if(data.compareTo(item) < 0) {
-    		if(left.getData()==null) {
-    			return false;
-    			
-    		}
-    		data = left.getData(); 
-    		contains(item);
-    	}else {
-    		if(right.getData()==null) {
-    			return false;
-    		}
-    		data = right.getData(); 
-    		contains(item);
-    	}
-        return false;
-    }
+	// TODO: est-ce que l'item fais partie du noeuds courant
+	// O(log(n))
+	public boolean contains(T item) {
+		int difference = item.compareTo(data);
+		if (difference == 0)
+			return true;
+		if (difference < 0 && left != null && left.contains(item))
+			return true;
+		if (difference > 0 && right != null && right.contains(item))
+			return true;
+		return false;
+	}
 
-    // TODO: on retourne la maximale de l'arbre
-    // O(n)
-    public int getHeight() {
-    	if( data == null )
-    		return -1;
-    	else
-    		return 1 + Math.max(left.getHeight(),right.getHeight());
-    }
+	// TODO: on retourne la maximale de l'arbre
+	// O(n)
+	public int getHeight() {
+		
+		  //if( data == null ) return -1;
+		  //else return 1 + Math.max(left.getHeight(),right.getHeight());
+		 
+		if (data == null)
+			return -1;
+		else {
+			if (right != null && left != null)
+				return 1 + Math.max(right.getHeight(), left.getHeight());
+			else {
+				if (right != null)
+					return 1 + right.getHeight();
+				else if (left != null)
+					return 1 + left.getHeight();
+				else
+					return 0;
+			}
+		}
+	}
 
-    // TODO: l'ordre d'insertion dans la liste est l'ordre logique
-    // de maniÃ¨re que le plus petit item sera le premier inserÃ©
-    // O(n)
-    public void fillListInOrder(List<BinaryNode<T>> result) {
+	// TODO: l'ordre d'insertion dans la liste est l'ordre logique
+	// de manière que le plus petit item sera le premier inseré
+	// O(n)
+	public void fillListInOrder(List<BinaryNode<T>> result) {
+		if (left != null) {
+			left.fillListInOrder(result);
+		}
+		result.add(this);
+		if (right != null) {
+			right.fillListInOrder(result);
+		}
+	}
 
-    	for() {
-    		
-    	}
-    	
-    }
 }
--- tp3/CompanyNode.java
+++ tp3/CompanyNode.java
@@ -1,42 +1,49 @@
 import java.util.List;
 
 public class CompanyNode implements Comparable<CompanyNode> {
-    private Integer money;
-    private BinarySearchTree<CompanyNode> childs;
-    public CompanyNode worstChild;
+	private Integer money;
+	private BinarySearchTree<CompanyNode> childs;
+	public CompanyNode worstChild;
 
-    // TODO: initialisation
-    // O(1)
-    public CompanyNode(Integer data) {
+	// TODO: initialisation
+	// O(1)
+	public CompanyNode(Integer data) {
+		money = data;
+		childs = new BinarySearchTree<CompanyNode>(this);
+		worstChild = this;
+	}
 
-    }
+	// TODO: la compagnie courante achete une autre compagnie
+	// O(log(n))
+	public void buy(CompanyNode item) {
+		childs.insert(item);
+		money = item.money;
+		if (worstChild.compareTo(item.worstChild) == 1) {
+			worstChild = item.worstChild;
+		}
+	}
 
-    // TODO: la compagnie courante achete une autre compagnie
-    // O(log(n))
-    public void buy(CompanyNode item) {
+	// TODO: on retourne le montant en banque de la compagnie
+	// O(1)
+	public Integer getMoney() {
+		return money;
+	}
 
-    }
+	// TODO: on rempli le builder de la compagnie et de ses enfants avec le format
+	// A
+	// > A1
+	// > A2
+	// > > A21...
+	// les enfants sont afficher du plus grand au plus petit (voir
+	// TestCompany.testPrint)
+	// O(n)
+	public void fillStringBuilderInOrder(StringBuilder builder, String prefix) {
+		
+	}
 
-    // TODO: on retourne le montant en banque de la compagnie
-    // O(1)
-    public Integer getMoney() {
-        return Integer.MIN_VALUE;
-    }
-
-    // TODO: on rempli le builder de la compagnie et de ses enfants avec le format
-    //A
-    // > A1
-    // > A2
-    // >  > A21...
-    // les enfants sont afficher du plus grand au plus petit (voir TestCompany.testPrint)
-    // O(n)
-    public void fillStringBuilderInOrder(StringBuilder builder, String prefix) {
-
-    }
-
-    // TODO: on override le comparateur pour defenir l'ordre
-    @Override
-    public int compareTo(CompanyNode item) {
-        return 0;
-    }
+	// TODO: on override le comparateur pour defenir l'ordre
+	@Override
+	public int compareTo(CompanyNode item) {
+		return 0;
+	}
 }
