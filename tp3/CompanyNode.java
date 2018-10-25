e05f17a178cd82fa5b8c1c8e9e68b5880731f90b
VINCENT TESSIER <vincetess9@gmail.com>
2018-10-24 20:57:09 -0400

    max

--- tp3/CompanyNode.java
+++ tp3/CompanyNode.java
@@ -41,20 +41,11 @@ public class CompanyNode implements Comparable<CompanyNode> {
 	public void fillStringBuilderInOrder(StringBuilder builder, String prefix) {
 		builder.append(prefix + this.getMoney() + "\n");
 		List<BinaryNode<CompanyNode>> list = childs.getItemsInOrder();
-
-		for (int i = list.size() - 1 ; i >=0; i--) {
-			try {
-			list.get(i).getData().fillStringBuilderInOrder(builder, prefix);
-			}catch(Exception e) {
-				
-			}
-
-/*
 		prefix = prefix + " > ";
 		for (int i = list.size() - 1; i >= 0; i--) {
 			if (list.get(i).getData().getMoney() != this.getMoney())
 				list.get(i).getData().fillStringBuilderInOrder(builder, prefix);
-*/
+
 		}
 	}
 
