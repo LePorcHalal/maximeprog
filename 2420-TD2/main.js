4b83609e39055a2a2553ec267f9c276fb97ff36d
Vincent Angell <vangell@videotron.ca>
2018-11-23 19:29:22 -0500

    Update affichage

--- 2420-TD2/main.js
+++ 2420-TD2/main.js
@@ -1,7 +1,7 @@
 //wt.base = "ws://log2420-nginx.info.polymtl.ca/chatservice";
 //endPoint = url_base?username={ nom };
 //wt client = new WebSocket(endPoint);
-  //var client = new WebSocket("ws://log2420-nginx.info.polymtl.ca/chatservice");
+//var client = new WebSocket("ws://log2420-nginx.info.polymtl.ca/chatservice");
 
 
 var currentChannelId;
@@ -11,193 +11,199 @@ var nomUsername = "Caporal Maxime"
 
 class Main {
 
-  constructor(){
+  constructor() {
     const self = this;
     self.initMain();
 
   }
 
-initMain(){
+  initMain() {
 
-const self = this;
-  this.connectHandlerObservable = new connectionHandler(nomUsername);
+    const self = this;
+    this.connectHandlerObservable = new connectionHandler(nomUsername);
 
-  this.channelsObs = new channelsObserver(this);
+    this.channelsObs = new channelsObserver(this);
 
-  this.messagesObs = new messagesObserver();
+    this.messagesObs = new messagesObserver();
 
-  this.connectHandlerObservable.addChannelsObserver(this.channelsObs);
-  this.connectHandlerObservable.addMessagesObserver(this.messagesObs)
+    this.connectHandlerObservable.addChannelsObserver(this.channelsObs);
+    this.connectHandlerObservable.addMessagesObserver(this.messagesObs)
 
-  this.connectHandlerObservable.connection();
+    this.connectHandlerObservable.connection();
 
-  self.initEventlisteners();
+    self.initEventlisteners();
 
-}
+  }
 
-initEventlisteners(){
-  const self = this;
+  initEventlisteners() {
+    const self = this;
 
-  //Event du bouton enter
-  document.getElementById("btnEnter").addEventListener("click", function(){
+    //Event du bouton enter
+    document.getElementById("btnEnter").addEventListener("click", function () {
 
       self.sendText();
 
-  });
-  //Event touche enter
-  document.addEventListener('keydown', (event) => {
-    const nomTouche = event.key;
+    });
+    //Event touche enter
+    document.addEventListener('keydown', (event) => {
+      const nomTouche = event.key;
 
-    if (nomTouche === 'Enter') {
-    self.sendText();
-    }
-
-  }, false);
-  //Event boutonAjouterChannel
-  document.getElementById("btnNouveauChannel").addEventListener("click", function(){
-
-    self.addChannel();
-
-  });
-  document.getElementById("btnNouveauChannel").addEventListener("click", function(){
-
-    self.addChannel();
-
-  });
-}
-
-addChannel(){
- const socketChannel = new Message("onCreateChannel","", document.getElementById("nomNouveauChannelID").value,nomUsername, Date.now());
- this.connectHandlerObservable.webSocket.send(JSON.stringify(socketChannel));
- document.getElementById("nomNouveauChannelID").value = "";
-}
-
-sendText() {
-
-  //obtention du texte
-     var data = document.getElementById("textBoxInput").value;
-     const socketMessage = new Message("onMessage", currentChannelId, data,"MAXIME", Date.now());
-      console.log("Message sent to socket : \n" + JSON.stringify(socketMessage));
-      this.connectHandlerObservable.webSocket.send(JSON.stringify(socketMessage));
-
-      //reset du texte box
-      document.getElementById("textBoxInput").value = "";
-
-    }
-    changeJoinStatus(channel,channelGeneral){
-
-      const self = this;
-
-      if(channel.joinStatus){
-
-        channel.joinStatus=false;
-        self.leaveChannel(channel.id,channelGeneral.id);
-
-      }else{
-
-        channel.joinStatus=true;
-        self.joinNewChannel(channel.id);
-
+      if (nomTouche === 'Enter') {
+        self.sendText();
       }
+
+    }, false);
+    //Event boutonAjouterChannel
+    document.getElementById("btnNouveauChannel").addEventListener("click", function () {
+
+      self.addChannel();
+
+    });
+    document.getElementById("btnNouveauChannel").addEventListener("click", function () {
+
+      self.addChannel();
+
+    });
+  }
+
+  addChannel() {
+    const socketChannel = new Message("onCreateChannel", "", document.getElementById("nomNouveauChannelID").value, nomUsername, Date.now());
+    this.connectHandlerObservable.webSocket.send(JSON.stringify(socketChannel));
+    document.getElementById("nomNouveauChannelID").value = "";
+  }
+
+  sendText() {
+
+    //obtention du texte
+    var data = document.getElementById("textBoxInput").value;
+    const socketMessage = new Message("onMessage", currentChannelId, data, "MAXIME", Date.now());
+    console.log("Message sent to socket : \n" + JSON.stringify(socketMessage));
+    this.connectHandlerObservable.webSocket.send(JSON.stringify(socketMessage));
+
+    //reset du texte box
+    document.getElementById("textBoxInput").value = "";
+
+  }
+  changeJoinStatus(channel, channelGeneral) {
+
+    const self = this;
+
+    if (channel.joinStatus) {
+
+      channel.joinStatus = false;
+      self.leaveChannel(channel.id, channelGeneral.id);
+
+    } else {
+
+      channel.joinStatus = true;
+      self.joinNewChannel(channel.id);
+
+    }
 
 
   }
-  setChannelList(){
+  setChannelList() {
 
     var symbole;
     const self = this;
     for (var i = 0; i < channelList.length; i++) {
 
-        var newDiv = document.createElement("div");
-        var newContent = document.createTextNode(channelList[i].name);
-        newDiv.appendChild(newContent);
-        newDiv.classList.add("channelItem");
-        newDiv.setAttribute("id",channelList[i].id);
-        document.getElementsByClassName("flex-channelList-container")[0].appendChild(newDiv);
+      var newDiv = document.createElement("div");
+      var newContent = document.createTextNode(channelList[i].name);
+      newDiv.appendChild(newContent);
+      if (i % 2 == 1) {
+        newDiv.style.backgroundColor = "#FFFFFF";
+      }
+      else {
+        newDiv.style.backgroundColor = "#BBB";
+      }
+      newDiv.classList.add("channelItem");
+      newDiv.setAttribute("id", channelList[i].id);
+      document.getElementsByClassName("flex-channelList-container")[0].appendChild(newDiv);
 
-       if(channelList[i].name == "Général"){
-          symbole = "glyphicon-star";
-        }else if(channelList[i].joinStatus){
-          symbole = "glyphicon-minus";
-        }else if(!(channelList[i].joinStatus)){
-          symbole = "glyphicon-plus";
-        }
+      if (channelList[i].name == "Général") {
+        symbole = "glyphicon-star";
+      } else if (channelList[i].joinStatus) {
+        symbole = "glyphicon-minus";
+      } else if (!(channelList[i].joinStatus)) {
+        symbole = "glyphicon-plus";
+      }
 
-          var symboleChannel  =   '<span class="pull-right channelBoutton">'+
-                                    '<a href="#" class="btn btn-lg btn-primary">'+
-                                      '<span class="glyphicon ' + symbole + '"></span>'+
-                                    '</a>'+
-                                   '</span>';
+      var symboleChannel = '<span class="pull-right channelBoutton">' +
+        '<a href="#" class="btn btn-lg btn-primary">' +
+        '<span class="glyphicon ' + symbole + '"></span>' +
+        '</a>' +
+        '</span>';
 
-        $(newDiv).append(symboleChannel);
+      $(newDiv).append(symboleChannel);
 
-        var classnameButton = document.getElementsByClassName("channelBoutton");
-        var classnameDiv = document.getElementsByClassName("channelItem");
-        var classnameChannelItem = document.getElementsByClassName("channelItem");
+      var classnameButton = document.getElementsByClassName("channelBoutton");
+      var classnameDiv = document.getElementsByClassName("channelItem");
+      var classnameChannelItem = document.getElementsByClassName("channelItem");
 
-        classnameButton[i].addEventListener('click',  function(){
+      classnameButton[i].addEventListener('click', function () {
 
 
 
         for (var i = 0; i < channelList.length; i++) {
 
-          if(channelList[i].id == event.currentTarget.parentElement.id){
-            if(!(channelList[i].name == "Général")){
-              self.changeJoinStatus(channelList[i],channelList[0]);
+          if (channelList[i].id == event.currentTarget.parentElement.id) {
+            if (!(channelList[i].name == "Général")) {
+              self.changeJoinStatus(channelList[i], channelList[0]);
             }
             break;
           }
 
         }
 
-        });
+      });
 
 
-        classnameDiv[i].addEventListener('click',  function(){
+      classnameDiv[i].addEventListener('click', function () {
 
-          if(event.target !== this){
-            return;
-          }
+        if (event.target !== this) {
+          return;
+        }
 
-          for (var i = 0; i < channelList.length; i++) {
+        for (var i = 0; i < channelList.length; i++) {
 
-            if(channelList[i].id == event.currentTarget.id){
-              if(channelList[i].joinStatus){
-                self.joinChannel(channelList[i].id);
-              }
-              break;
+          if (channelList[i].id == event.currentTarget.id) {
+            if (channelList[i].joinStatus) {
+              self.joinChannel(channelList[i].id);
             }
-
+            break;
           }
 
+        }
 
-        });
 
-      }
-}
-  joinChannel(channelId){
+      });
+
+    }
+  }
+  joinChannel(channelId) {
     const self = this
-    const socketGetChannel = new Message("onGetChannel",channelId,"",nomUsername, Date.now());
+    const socketGetChannel = new Message("onGetChannel", channelId, "", nomUsername, Date.now());
     this.connectHandlerObservable.webSocket.send(JSON.stringify(socketGetChannel));
   }
 
-  joinNewChannel(channelId){
+  joinNewChannel(channelId) {
     const self = this;
-    const socketJoinChannel = new Message("onJoinChannel",channelId,"",nomUsername, Date.now());
+    const socketJoinChannel = new Message("onJoinChannel", channelId, "", nomUsername, Date.now());
     this.connectHandlerObservable.webSocket.send(JSON.stringify(socketJoinChannel));
     self.joinChannel(channelId);
 
 
   }
-  leaveChannel(channelId,channelGeneralId){
-const self = this
-    const socketLeaveChannel = new Message("onLeaveChannel",channelId,"",nomUsername, Date.now());
+  leaveChannel(channelId, channelGeneralId) {
+    const self = this
+    const socketLeaveChannel = new Message("onLeaveChannel", channelId, "", nomUsername, Date.now());
     this.connectHandlerObservable.webSocket.send(JSON.stringify(socketLeaveChannel));
 
-    const socketGetChannel = new Message("onGetChannel",channelGeneralId,nomUsername, Date.now());
+    const socketGetChannel = new Message("onGetChannel", channelGeneralId, nomUsername, Date.now());
     this.connectHandlerObservable.webSocket.send(JSON.stringify(socketGetChannel));
 
 
 
-}
+  }
 }
